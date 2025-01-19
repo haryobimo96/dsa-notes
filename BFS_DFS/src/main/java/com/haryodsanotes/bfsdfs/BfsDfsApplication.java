07c597b806b7dce325f52cb3e8fd33c519b5a945
Haryo Bimo Cokrokusumo <haryoc5@gmail.com>
2025-01-19 15:27:54 +0700

    initial commit'

---
+++ BFS_DFS/src/main/java/com/haryodsanotes/bfsdfs/BfsDfsApplication.java
@@ -0,0 +1,13 @@
+package com.haryodsanotes.bfsdfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfsDfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfsDfsApplication.class, args);
+	}
+
+}
---
+++ BFS_DFS/src/test/java/com/haryodsanotes/bfsdfs/BfsDfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haryodsanotes.bfsdfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfsDfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
