d30605105f3690a14fda929a76ce1804765fec4c
Haryo Bimo Cokrokusumo <haryoc5@gmail.com>
2025-01-19 16:25:44 +0700

    add exception and its handler for adjacency matrix dimension check

---
+++ BFS_DFS/src/main/java/com/haryodsanotes/bfsdfs/exceptions/MatrixDimensionException.java
@@ -0,0 +1,11 @@
+package com.haryodsanotes.bfsdfs.exceptions;
+
+public class MatrixDimensionException extends RuntimeException {
+    public MatrixDimensionException() {
+        super();
+    }
+
+    public MatrixDimensionException(String message) {
+        super(message);
+    }
+}
---
+++ BFS_DFS/src/main/java/com/haryodsanotes/bfsdfs/exceptions/ReturnedExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.haryodsanotes.bfsdfs.exceptions;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class ReturnedExceptionHandler {
+    @ExceptionHandler(MatrixDimensionException.class)
+    public ResponseEntity<String> handleMatrixDimensionException(MatrixDimensionException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+}
